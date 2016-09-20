61b93cf5b82c677d25ce7574693233736c3e301a
RavindraKhati <rk6@ipaccess.com>
2016-09-20 15:52:43 +0100

    characterisation versioning

--- board/ipaccess/ipat2k/characterisation.c
+++ board/ipaccess/ipat2k/characterisation.c
@@ -266,23 +266,50 @@ void deserialise_characterisation_info_eeprom(const uint8_t payload[CONFIG_CHARA
     cd->oui[0] = payload[17];
     cd->oui[1] = payload[18];
     cd->oui[2] = payload[19];
-    cd->serial = ((((uint64_t)(payload[20])) << 32) & 0x300000000) | 
-	    ((((uint64_t)(payload[21])) << 24) & 0xFF000000) |
-	    ((((uint64_t)(payload[22])) << 16) & 0x00FF0000) |
-	    ((((uint64_t)(payload[23])) <<  8) & 0x0000FF00) |
-	    ((((uint64_t)(payload[24])) <<  0) & 0x000000FF);
 
-    if ( (payload[239] & 0xE1) == 0x80)
-	    cd->production_mode = 1;
-    else if ((payload[239] & 0xE1) == 0x40)
-	    cd->development_mode = 1;
-    else if ( (payload[239] & 0xE1) == 0x20)
-	    cd->specials_mode = 1;
-    else if ( (payload[239] & 0xE1) == 0x01)
-	    cd->test_mode = 1;
-    else 
-	    /*nothing set: Consider specials in case of eeprom characterisation*/
-	    cd->specials_mode = 1;
+    if (cd->version == 0)
+    {
+	    cd->serial = ((((uint64_t)(payload[20])) << 24) & 0xFF000000) |
+		    ((((uint64_t)(payload[21])) << 16) & 0x00FF0000) |
+		    ((((uint64_t)(payload[22])) <<  8) & 0x0000FF00) |
+		    ((((uint64_t)(payload[23])) <<  0) & 0x000000FF);
+
+	    if ((payload[239] & 0x2) == 0x2)
+	    {
+		    if ((payload[239] & 0x1) == 0x1)
+		    {
+			    cd->test_mode = 1;
+		    }
+		    else
+		    {
+			    cd->development_mode = 1;
+		    }
+	    }
+	    else
+	    {
+		    cd->specials_mode = 1;
+	    }
+
+    }
+    else
+    {
+	    cd->serial = ((((uint64_t)(payload[20])) << 32) & 0x300000000) | 
+		    ((((uint64_t)(payload[21])) << 24) & 0xFF000000) |
+		    ((((uint64_t)(payload[22])) << 16) & 0x00FF0000) |
+		    ((((uint64_t)(payload[23])) <<  8) & 0x0000FF00) |
+		    ((((uint64_t)(payload[24])) <<  0) & 0x000000FF);
+
+	    if ( (payload[239] & 0xE1) == 0x80)
+		    cd->production_mode = 1;
+	    else if ((payload[239] & 0xE1) == 0x40)
+		    cd->development_mode = 1;
+	    else if ( (payload[239] & 0xE1) == 0x20)
+		    cd->specials_mode = 1;
+	    else if ( (payload[239] & 0xE1) == 0x01)
+		    cd->test_mode = 1;
+	    else 
+		    cd->production_mode = 1;
+    }
 
 }
 
@@ -1113,7 +1140,7 @@ int do_characterise(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
             cd.version = CONFIG_CHARACTERISATION_IPAT2K_VERSION;
             serialise_characterisation_info_eeprom(&cd, serialised);
 
-            /* write the EEPROM in page mode (8 bytes at a time) */
+            /* write the EEPROM in page mode (256 bytes at a time) */
             for (i = 0; i < CONFIG_CHARACTERISATION_IPAT2K_SIZE; i += 256)
             {
                 if (0 != (ret = i2c_write(CONFIG_CHARACTERISATION_EEPROM_ADDR, CONFIG_CHARACTERISATION_IPAT2K_OFFSET + i, 1, serialised + i, 256)))
